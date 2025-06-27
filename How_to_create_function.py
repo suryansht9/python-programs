268d1bfe7e7b6f4e3f240eef0609332556516893
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-06-27 07:35:38 +0530

    Add files via upload

---
+++ How_to_create_function.py
@@ -0,0 +1,21 @@
+#✅ 1. Basic Syntax of a Function
+
+def function_name():
+    # code to execute
+    print("This is my function.")
+'''💡 Explanation:
+def is a keyword that means “define a function.”
+
+function_name is any name you give to the function.
+
+() is for parameters (we’ll talk about them below).
+'''
+#✅ 2. Function with Parameters
+#Parameters let you pass data into the function.
+
+def greet(name):
+    print(f"Hello, {name}!")
+✅ Calling the function:
+greet("Suryansh")
+🖨 Output:
+Hello, Suryansh!
